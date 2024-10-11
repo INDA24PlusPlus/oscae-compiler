b80d5a26bba71f0e8c1affffd530fcb7589718cb
Oscar Eriksson <oscareriksson2004@hotmail.com>
2024-10-11 17:17:10 +0200

    Added a static parser class with a function Parse that does you can call instead of creating an AST from the constructor.

---
+++ Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oscae_compiler
+{
+    static class Parser
+    {
+        public static AbstractSyntaxTree Parse(List<Token> tokens)
+        {
+            return new AbstractSyntaxTree(tokens);
+        }
+    }
+}
